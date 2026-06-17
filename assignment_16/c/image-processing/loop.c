2c73c8772edb1636c65a24edb89d93a2a34203ac
BaraldsBR <baraldsbr@gmail.com>
2026-06-17 13:21:51 +0200

    use separate pipelines

--- assignment_16/c/image-processing/loop.c
+++ assignment_16/c/image-processing/loop.c
@@ -13,8 +13,8 @@
 #include "loop.h"
 #include "image-processing.h"
 
-static GstElement* stream_source;
-static bool send_data = true;
+static GstElement *stream_pipeline, *stream_source;
+static bool send_data = true, stream_started = false;
 
 static GstFlowReturn new_sample (GstElement *sink) {
   printf("new_sample() ran\n");
@@ -63,6 +63,12 @@ static GstFlowReturn new_sample (GstElement *sink) {
     }
 
     if (send_data) {
+      if (!stream_started) {
+        stream_started = true;
+        g_print ("Now streaming:\n");
+        gst_element_set_state (stream_pipeline, GST_STATE_PLAYING);
+      }
+
       size_t buffer_size = IMAGE_HEIGHT * IMAGE_WIDTH * 2;
 
       GstBuffer *buffer_out = gst_buffer_new_and_alloc(buffer_size);
@@ -132,12 +138,16 @@ void* imageProcessingLoop (void* args)
 {
   GMainLoop *loop;
 
-  GstElement *pipeline, *source, *sink;
-  // stream_source is static so it can accesed from the new_sample function
+  GstElement *process_pipeline, *source, *sink;
+  GstBus *process_bus;
+  guint process_bus_watch_id;
+  
+  // stream_pipeline and stream_source are static so they can accesed from the new_sample function
   GstElement *stream_jpegenc, *stream_rtpenc, *stream_sink;
+  GstBus *stream_bus;
+  guint stream_bus_watch_id;
+
   GstCaps *caps;
-  GstBus *bus;
-  guint bus_watch_id;
 
   // /* Initialisation */
   gst_init (NULL, NULL);
@@ -147,7 +157,8 @@ void* imageProcessingLoop (void* args)
   // /* Check input arguments */
 
   /* Create gstreamer elements */
-  pipeline   = gst_pipeline_new ("");
+  process_pipeline   = gst_pipeline_new ("");
+  stream_pipeline    = gst_pipeline_new ("");
 
   /* Image Processing */
   source      = gst_element_factory_make ("v4l2src",       "webcam-source");
@@ -158,26 +169,37 @@ void* imageProcessingLoop (void* args)
   stream_rtpenc  = gst_element_factory_make ("rtpjpegpay", "stream-rtpenc");
   stream_sink    = gst_element_factory_make ("udpsink",    "stream-sink");
 
-  if (!pipeline || !source || !sink) {
-    g_printerr ("One element could not be created. Exiting.\n");
+  if (!process_pipeline || !source || !sink) {
+    g_printerr ("One processing element could not be created. Exiting.\n");
     return NULL;
   }
 
-  /* Set up the pipeline */
+  if (!stream_pipeline || !stream_source || !stream_jpegenc || !stream_rtpenc || !stream_sinksink) {
+    g_printerr ("One streaming element could not be created. Exiting.\n");
+    return NULL;
+  }
+
+  /* Set up the process_pipeline */
 
   g_object_set (G_OBJECT (source), "device", DEVICE_NAME, NULL);
   g_object_set (G_OBJECT (sink), "emit-signals", TRUE, NULL);
   g_signal_connect (G_OBJECT (sink), "new-sample", G_CALLBACK(new_sample), NULL);
+
+  /* Set up the stream_pipeline */
 
   g_object_set (G_OBJECT (stream_sink), "host", STREAM_IP, NULL);
   g_object_set (G_OBJECT (stream_sink), "port", STREAM_PORT, NULL);
   g_signal_connect(stream_source, "need-data", G_CALLBACK(startFeed), NULL);
   g_signal_connect(stream_source, "enough-data", G_CALLBACK(stopFeed), NULL);
 
-  /* we add a message handler */
-  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
-  gst_object_unref (bus);
+  /* we add message handlers */
+  process_bus = gst_pipeline_get_bus (GST_PIPELINE (process_pipeline));
+  process_bus_watch_id = gst_bus_add_watch (process_bus, bus_call, loop);
+  gst_object_unref (process_bus);
+
+  stream_bus = gst_pipeline_get_bus (GST_PIPELINE (stream_pipeline));
+  stream_bus_watch_id = gst_bus_add_watch (stream_bus, bus_call, loop);
+  gst_object_unref (stream_bus);
 
   /* Define capabilities */
   caps = gst_caps_new_simple ("video/x-raw",
@@ -187,23 +209,26 @@ void* imageProcessingLoop (void* args)
                               "framerate", GST_TYPE_FRACTION, VIDEO_FPS, 1,
                               NULL);
 
-  /* we add all elements into the pipeline */
-  gst_bin_add_many (GST_BIN (pipeline),
-                    source, sink,
+  /* we add all elements into the process_pipeline */
+  gst_bin_add_many (GST_BIN (process_pipeline),
+                    source, sink, NULL);
+
+  gst_element_link_filtered (source, sink, caps);
+
+  
+  gst_bin_add_many (GST_BIN (stream_pipeline),
                     stream_source, stream_jpegenc,
                     stream_rtpenc, stream_sink, NULL);
 
-  gst_element_link_filtered (source, sink, caps);
-  
   gst_element_link_filtered (stream_source, stream_jpegenc, caps);
   gst_element_link (stream_jpegenc, stream_rtpenc);
   gst_element_link (stream_rtpenc, stream_sink);
 
   gst_caps_unref(caps);
 
-  /* Set the pipeline to "playing" state*/
+  /* Set the process_pipeline to "playing" state*/
   g_print ("Now recording:\n");
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  gst_element_set_state (process_pipeline, GST_STATE_PLAYING);
 
 
   /* Iterate */
@@ -212,11 +237,17 @@ void* imageProcessingLoop (void* args)
 
   /* Out of the main loop, clean up nicely */
   g_print ("Returned, stopped recording\n");
-  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_element_set_state (process_pipeline, GST_STATE_NULL);
+  gst_element_set_state (stream_pipeline, GST_STATE_NULL);
 
-  g_print ("Deleting pipeline\n");
-  gst_object_unref (GST_OBJECT (pipeline));
-  g_source_remove (bus_watch_id);
+  g_print ("Deleting process_pipeline\n");
+  gst_object_unref (GST_OBJECT (process_pipeline));
+  g_source_remove (process_bus_watch_id);
+  g_main_loop_unref (loop);
+
+  g_print ("Deleting stream_pipeline\n");
+  gst_object_unref (GST_OBJECT (stream_pipeline));
+  g_source_remove (stream_bus_watch_id);
   g_main_loop_unref (loop);
 
   return NULL;
