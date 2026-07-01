9884f0195245bdbb57d87ac25b1f46945c00839e
Alexandre Lopez <alexxandre.lopez@gmail.com>
2026-07-02 00:07:29 +0200

    Made the TopEntity more readable. Compiles fine

--- assignment_16/verilog/TopEntity.v
+++ assignment_16/verilog/TopEntity.v
@@ -9,14 +9,14 @@ module TopEntity(
   input btn1,
   output led1,
 
-  // Pitch Motor
+  // Pitch
   input  PITCH_ENC_A,
   input  PITCH_ENC_B,
   output PITCH_DIRA,
   output PITCH_DIRB,
   output PITCH_PWM_VAL,
   
-  // Yaw Motor
+  // Yaw
   input  YAW_ENC_A,
   input  YAW_ENC_B,
   output YAW_DIRA,
@@ -29,12 +29,29 @@ module TopEntity(
   input  SPI_CS,
   output SPI_POCI
 );
-  wire        rst = btn1;
-  assign      led1 = rst;
+  wire   rst = btn1;
+  assign led1 = rst;
 
-  // unscramble
-  wire [15:0] target_pitch = { rx_buf[23:16], rx_buf[31:24] };
-  wire [15:0] target_yaw = { rx_buf[7:0], rx_buf[15:8] };
+  // SPI module
+  wire [31:0] rx_buf;
+  wire [31:0] tx_buf;
+
+  spi spi (
+    .rst(rst),
+    .clk(clk),
+    .sck(SPI_CLK),
+    .mosi(SPI_PICO),
+    .cs(SPI_CS),
+    .miso(SPI_POCI),
+    .data_out(rx_buf),
+    .new_data(),
+    .data_in(tx_buf)
+  );
+
+  // PWM module
+  // Descramble SPI coming from RPi
+  wire [15:0] target_pitch = { rx_buf[23:16] , rx_buf[31:24] };
+  wire [15:0] target_yaw   = { rx_buf[7:0]   , rx_buf[15:8]  };
 
   pwm pitch_pwm (
     .rst(rst),
@@ -54,76 +71,61 @@ module TopEntity(
     .PWM_VAL(YAW_PWM_VAL)
   );
 
-  wire signed [15:0] pitch_out;
-  wire signed [15:0] yaw_out;
-
-  wire debounced_pitch_enc_A;
-  wire debounced_pitch_enc_B;
-  wire debounced_yaw_enc_A;
-  wire debounced_yaw_enc_B;
+  // Debounce encoder inputs
+  wire debounced_enc_A_pitch;
+  wire debounced_enc_B_pitch;
+  wire debounced_enc_A_yaw;
+  wire debounced_enc_B_yaw;
 
   debouncer debouncer_pitch_A (
     .clk(clk),
     .rst(rst),
     .signal(PITCH_ENC_A),
-    .debounced(debounced_pitch_enc_A)
+    .debounced(debounced_enc_A_pitch)
   );
 
   debouncer debouncer_pitch_B (
     .clk(clk),
     .rst(rst),
     .signal(PITCH_ENC_B),
-    .debounced(debounced_pitch_enc_B)
+    .debounced(debounced_enc_B_pitch)
   );
 
   debouncer debouncer_yaw_A (
     .clk(clk),
     .rst(rst),
     .signal(YAW_ENC_A),
-    .debounced(debounced_yaw_enc_A)
+    .debounced(debounced_enc_A_yaw)
   );
 
   debouncer debouncer_yaw_B (
     .clk(clk),
     .rst(rst),
     .signal(YAW_ENC_B),
-    .debounced(debounced_yaw_enc_B)
+    .debounced(debounced_enc_B_yaw)
   );
-  
-  encoder pitch_encoder (
+
+  // Encoder module
+  wire signed [15:0] pitch_out;
+  wire signed [15:0] yaw_out;
+
+  encoder encoder_pitch (
     .rst(rst),
     .clk(clk),
-    .signal_A(debounced_pitch_enc_A),
-    .signal_B(debounced_pitch_enc_B),
+    .signal_A(debounced_enc_A_pitch),
+    .signal_B(debounced_enc_B_pitch),
     .pos_out(pitch_out) 
   );
   
-  encoder yaw_encoder (
+  encoder encoder_yaw (
     .rst(rst),
     .clk(clk),
-    .signal_A(debounced_yaw_enc_A),
-    .signal_B(debounced_yaw_enc_B),
+    .signal_A(debounced_enc_A_yaw),
+    .signal_B(debounced_enc_B_yaw),
     .pos_out(yaw_out) 
   );
 
-  wire [31:0] rx_buf;
-  wire [31:0] tx_buf = {
-    pitch_out[7:0],
-    pitch_out[15:8],
-    yaw_out[7:0],
-    yaw_out[15:8]
-  };
-
-  spi spi (
-    .rst(rst),
-    .clk(clk),
-    .sck(SPI_CLK),
-    .mosi(SPI_PICO),
-    .cs(SPI_CS),
-    .miso(SPI_POCI),
-    .data_out(rx_buf),
-    .new_data(),
-    .data_in(tx_buf)
-  );
+  // Pre-scramble SPI content for RPi
+  assign tx_buf= {pitch_out[7:0], pitch_out[15:8], yaw_out[7:0], yaw_out[15:8]};
 
 endmodule
