59092e8c5398633fa136e454d0e0a2181980d6fe
Alexandre Lopez <alexxandre.lopez@gmail.com>
2026-07-01 20:37:19 +0200

    Prettified encoder by hand, still compiles yay, untested

--- assignment_16/verilog/encoder.v
+++ assignment_16/verilog/encoder.v
@@ -1,49 +1,42 @@
 module encoder #(
-    parameter POS_WIDTH = 16
+  parameter POS_WIDTH = 16
 ) (
-    input rst,
-    input clk,
+  input rst,
+  input clk,
 
-    input signal_A,
-    input signal_B,
-    output wire [POS_WIDTH-1:0] pos_out // more than enough
+  input signal_A,
+  input signal_B,
+  output reg [POS_WIDTH-1:0] pos_out
 );
-    reg old_A;
-    reg old_B; 
-    reg signed [POS_WIDTH-1:0] position;
-    assign pos_out = position;
+  reg old_A;
+  reg old_B;
 
-    wire rising_A  = !old_A && signal_A;
-    wire falling_A = old_A  && !signal_A; 
-    wire rising_B  = !old_B && signal_B;
-    wire falling_B = old_B  && !signal_B;
-    
-    wire increment = (rising_A  &&  signal_B)
-                   || (falling_B &&  signal_A)
-                   || (falling_A && !signal_B)
-                   || (rising_B  && !signal_A);
-    
-    wire decrement = (rising_A  && !signal_B)
-                   || (rising_B  &&  signal_A)
-                   || (falling_A &&  signal_B)
-                   || (falling_B && !signal_A);
-                   
-    always @(posedge clk) begin
-        if(rst) begin
-            position <= 0;
-            old_A <= signal_A;
-            old_B <= signal_B;
-        end else begin
-            if (increment) begin
-                position <= position + 1;
-            end
-            if (decrement) begin
-                position <= position - 1;
-            end
+  wire rising_A  = !old_A && signal_A;
+  wire falling_A = old_A  && !signal_A; 
+  wire rising_B  = !old_B && signal_B;
+  wire falling_B = old_B  && !signal_B;
 
-            old_A <= signal_A;
-            old_B <= signal_B;
-        end
+  wire increment =  (rising_A  &&  signal_B)
+                 || (falling_B &&  signal_A)
+                 || (falling_A && !signal_B)
+                 || (rising_B  && !signal_A);
+
+  wire decrement =  (rising_A  && !signal_B)
+                 || (rising_B  &&  signal_A)
+                 || (falling_A &&  signal_B)
+                 || (falling_B && !signal_A);
+
+  always @(posedge clk) begin
+    if (rst) begin
+      pos_out <= 0;
+      old_A <= signal_A;
+      old_B <= signal_B;
+    end else begin
+      if (increment) pos_out <= pos_out + 1;
+      if (decrement) pos_out <= pos_out - 1;
+
+      old_A <= signal_A;
+      old_B <= signal_B;
     end
-
+  end
 endmodule
